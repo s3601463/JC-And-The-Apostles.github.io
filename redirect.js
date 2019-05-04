e391456da6ecf513dc25a2bbf3ac8d5d60f72853
s3601463 <48815380+s3601463@users.noreply.github.com>
2019-05-04 20:34:02 +1000

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,22 @@
+//Redirection Script
+function redirHP(){
+	window.location.assign("index.html");
+}
+function redirPI(){
+	window.location.assign("Project-Idea.html");
+}
+function redirID(){
+	window.location.assign("Industry-Data.html");
+}
+function redirIW(){
+	window.location.assign("IT-Work.html");
+}
+function redirIT(){
+	window.location.assign("IT-Technologies.html");
+}
+function redirGR(){
+	window.location.assign("Group-Reflection.html");
+}
+function redirTP(){
+	window.location.assign("Team-Profile.html");
+}
