7fafb216cadc7252c5783d3fd97a2de12860fde4
s3601463 <48815380+s3601463@users.noreply.github.com>
2019-05-06 02:21:00 +1000

    Add files via upload

--- redirect.js
+++ redirect.js
@@ -1,55 +1,55 @@
 //Redirection Script
 function redirHP(){
-	var x = Math.floor(Math.random()* 15)+1;
-	if (x == 4) {
+	var x = Math.floor(Math.random()* 19)+1;
+	if (x == 13) {
 		forced404();
 	} else {
 		window.location.assign("index.html");
 	}
 }
 function redirPI(){
-	var x = Math.floor(Math.random()* 3)+1;
-	if (x == 3) {
+	var x = Math.floor(Math.random()* 19)+1;
+	if (x == 13) {
 		forced404();
 	} else {
 		window.location.assign("Project-Ideas.html");
 	}
 }
 function redirID(){
-	var x = Math.floor(Math.random()* 3)+1;
-	if (x == 3) {
+	var x = Math.floor(Math.random()* 19)+1;
+	if (x == 13) {
 		forced404();
 	} else {
 		window.location.assign("Industry-Data.html");
 	}
 }
 function redirIW(){
-	var x = Math.floor(Math.random()* 3)+1;
-	if (x == 3) {
+	var x = Math.floor(Math.random()* 19)+1;
+	if (x == 13) {
 		forced404();
 	} else {
 		window.location.assign("IT-Work.html");
 	}
 }
 function redirIT(){
-	var x = Math.floor(Math.random()* 3)+1;
-	if (x == 3) {
+	var x = Math.floor(Math.random()* 19)+1;
+	if (x == 13) {
 		forced404();
 	} else {
 		window.location.assign("IT-Technologies.html");
 	}
 }
 function redirGR(){
-	var x = Math.floor(Math.random()* 3)+1;
-	if (x == 3) {
+	var x = Math.floor(Math.random()* 19)+1;
+	if (x == 13) {
 		forced404();
 	} else {
 		window.location.assign("Group-Reflection.html");
 	}
 }
 function redirTP(){
-	var x = Math.floor(Math.random()* 3)+1;
-	if (x == 3) {
+	var x = Math.floor(Math.random()* 19)+1;
+	if (x == 13) {
 		forced404();
 	} else {
 		window.location.assign("Team-Profile.html");
@@ -92,9 +92,81 @@ function avPG3(){
 function itCS(){
 	window.location.assign("ITT-CS-1.html");
 }
+function itCS2(){
+	window.location.assign("ITT-CS-2.html");
+}
+function itCS3(){
+	window.location.assign("ITT-CS-3.html");
+}
+function itCS4(){
+	window.location.assign("ITT-CS-4.html");
+}
+function itCS5(){
+	window.location.assign("ITT-CS-5.html");
+}
+function itCS6(){
+	window.location.assign("ITT-CS-6.html");
+}
+
 function itML(){
 	window.location.assign("ITT-ML-1.html");
 }
+function itML2(){
+	window.location.assign("ITT-ML-2.html");
+}
+function itML3(){
+	window.location.assign("ITT-ML-3.html");
+}
+
 function itRB(){
 	window.location.assign("ITT-RB-1.html");
+}
+function itRB2(){
+	window.location.assign("ITT-RB-2.html");
+}
+function itRB3(){
+	window.location.assign("ITT-RB-3.html");
+}
+function itRB4(){
+	window.location.assign("ITT-RB-4.html");
+}
+
+function tpPI(){
+ window.location.assign("Team-Profile-PI-1.html");
+}
+function tpPI2(){
+ window.location.assign("Team-Profile-PI-2.html");
+}
+
+function tpTP(){
+	window.location.assign("Team-Profile-TP-1.html");
+}
+function tpTP2(){
+	window.location.assign("Team-Profile-TP-2.html");
+}
+function tpTP3(){
+	window.location.assign("Team-Profile-TP-3.html");
+}
+
+function tpIJ(){
+	window.location.assign("Team-Profile-IJ.html");
+}
+
+function grFJ(){
+	window.location.assign("Group-Reflection-FJ.html");
+}
+function grJA(){
+	window.location.assign("Group-Reflection-JA.html");
+}
+function grJI(){
+	window.location.assign("Group-Reflection-JI.html");
+}
+function grJO(){
+	window.location.assign("Group-Reflection-JO.html");
+}
+function grMA(){
+	window.location.assign("Group-Reflection-MA.html");
+}
+function grMI(){
+	window.location.assign("Group-Reflection-MI.html");
 }
